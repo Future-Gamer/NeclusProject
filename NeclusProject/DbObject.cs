327f11b56b23c14e2fd11aafc047fff6f38cb9a8
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2025-03-26 19:17:33 +0530

    Added basic utilities

---
+++ NeclusProject/DbObject.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NucleusProject
+{
+    abstract public class DbObject
+    {
+        abstract public void Sync(string connectionString = null);
+    }
+
+    public class ScheduleData : DbObject
+    {
+        public DataSet dataSet;
+
+        public ScheduleData()
+        {
+            this.dataSet = new DataSet();
+        }
+        public override void Sync(string connectionString = null)
+        {
+            string connStr = connectionString ?? Values.ConnectionString;
+            const string cmd = @"SELECT Mst_Course.""Name"" AS ""Course"", Mst_Class.""Name"" AS ""Class"", E_Days.""Name"" AS ""Day"", E_Class_Status.""Name"" AS ""Status"", Mst_Faculty.""Name"" AS ""Faculty"", Mst_Faculty.""Email"" AS ""Email"", Mst_Faculty.""Phone"" AS ""Phone"", ""Start"", ""End"" FROM Trn_Schedule JOIN E_Class_Status ON Trn_Schedule.""Status""=E_Class_Status.Id JOIN E_Days ON Trn_Schedule.""Day""=E_Days.Id JOIN Mst_Course ON Trn_Schedule.""Course""=Mst_Course.Id JOIN Mst_Class ON Trn_Schedule.""Class""=Mst_Class.""Id"" JOIN Mst_Faculty ON Trn_Schedule.""Faculty""=Mst_Faculty.""Id"" ORDER BY Trn_Schedule.""Start"" ASC";
+            SqlConnection connection = new SqlConnection(connStr);
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                sqlCommand.CommandText = cmd;
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                adapter.Fill(dataSet);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+            return;
+        }
+    }
+
+    public class CourseData : DbObject
+    {
+        public DataSet dataSet;
+        public CourseData()
+        {
+            this.dataSet = new DataSet();
+        }
+        public override void Sync(string connectionString = null)
+        {
+            string connStr = connectionString ?? Values.ConnectionString;
+            const string cmd = "SELECT * FROM Mst_Course";
+            SqlConnection connection = new SqlConnection(connStr);
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                sqlCommand.CommandText = cmd;
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                adapter.Fill(dataSet);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+            return;
+        }
+    }
+
+    public class StudentData : DbObject
+    {
+        public StudentData()
+        {
+            this.dataSet = new DataSet();
+        }
+        public DataSet dataSet;
+        public override void Sync(string connectionString = null)
+        {
+            string connStr = connectionString ?? Values.ConnectionString;
+            const string cmd = "SELECT * FROM Mst_Student";
+            SqlConnection connection = new SqlConnection(connStr);
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                sqlCommand.CommandText = cmd;
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                adapter.Fill(dataSet);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+            return;
+        }
+    }
+}
---
+++ NeclusProject/Values.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NucleusProject
+{
+    public class Values
+    {
+        private static readonly string baseDir = @"C:\Users\jeffb\Source\Repos\NeclusProject\NeclusProject";
+
+        public static string ConnectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = @"(LocalDB)\MSSQLLocalDB";
+                builder.AttachDBFilename = baseDir + @"App_Data\NucleusDatabase.mdf";
+                builder.IntegratedSecurity = true;
+
+                return builder.ToString();
+            }
+        }
+
+    }
+}
